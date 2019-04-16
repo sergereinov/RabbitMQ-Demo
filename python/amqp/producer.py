58ec8535fe02aa206ebea602766383ffdc0f19b2
sergereinov <47950629+sergereinov@users.noreply.github.com>
2019-04-16 16:24:37 +0300

    add self.open() and self.process_data_events() (from BlockingConnection)

--- python/amqp/producer.py
+++ python/amqp/producer.py
@@ -10,16 +10,25 @@ import pika
 class Producer:
     def __init__(self, host, exchange_type, exchange_name):
         '''init producer'''
+        self.host = host
         self.exchange_name = exchange_name
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type=exchange_type)
+        self.exchange_type = exchange_type
+        self.open()
 
     def publish(self, msg, routing_key=''):
         '''publish message with routing_key'''
         bmsg = bytes(msg, "utf8")
         self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key, body=bmsg)
 
+    def open(self):
+        '''(re-)open connection'''
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type=self.exchange_type)
+    
     def close(self):
         '''cleanup connection'''
         self.connection.close()
+
+    def process_data_events(self):
+        self.connection.process_data_events()
